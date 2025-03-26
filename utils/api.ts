993a740f9cf2a6329b51fe3a0a23c64c96afc338
kennedymahoro <kennedymahoro1@gmail.com>
2025-03-26 16:05:23 -0400

    changed APIs

--- utils/api.ts
+++ utils/api.ts
@@ -1,6 +1,13 @@
 import axios from "axios";
 
+
 export const fetchTypingText = async (): Promise<string> => {
-  const response = await axios.get("https://baconipsum.com/api/?type=meat-and-filler&paras=1");
-  return response.data[0];
+  try {
+    const response = await axios.get("https://zenquotes.io/api/random");
+    const quote = response.data[0].q; // Extract quote text
+    return quote;
+  } catch (error) {
+    console.error("Error fetching quote:", error);
+    return "Failure is simply the opportunity to begin again, this time more intelligently. - Henry Ford";
+  }
 };
